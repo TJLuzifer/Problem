1dfca6233b0b4de151c894dd017b9e320004eb18
TJLuzifer <54908329+TJLuzifer@users.noreply.github.com>
2019-09-04 20:17:15 +0200

    Add files via upload

---
+++ testmod/items/ModBlocks.java
@@ -0,0 +1,9 @@
+package de.blutmondgilde.testmod.items;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+
+    @ObjectHolder("de.blutmodgilde.testmod:Metal")
+    public static de.blutmondgilde.testmod.blocks.Metal METAL;
+}
